7f429ba639ac6113d48a131ea614f3e1bda5a57c
Alex <alexey.kalyagin@ya.ru>
2022-08-13 18:04:33 +0300

    ближе к чистовику

--- Seminar3/seminar/Homework/чурновик/черновик.cs
+++ Seminar3/seminar/Homework/чурновик/черновик.cs
@@ -1,27 +1,30 @@
 ﻿// Задача2
 
+
 Console.WriteLine("Введите трехзначное число ");
 
 int numberB = Convert.ToInt32(Console.ReadLine());
 int numberA = 0;
 int numberC = 0;
-int numberX = 0;
-
-int [] array = {};  
-int index = 0;
-if (numberB < 100)
-{
-    Console.WriteLine("Третьей цифры нет");
-} 
 
 while (numberB > 0)
 {
     numberA = numberB % 10;
-    array[index] = numberA;
-    index++;  
     numberB = numberB / 10;
     numberC = (numberC * 10);
     numberC = numberC + numberA;
 }
 Console.WriteLine(numberC);
-Console.WriteLine(array[2]);
+
+void Array(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        if(collection[index] == 2);
+                
+        index++;
+    }
+
+}
