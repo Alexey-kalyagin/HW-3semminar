1580793c1607e1463f6f82c46601687c91d843a4
Alex <alexey.kalyagin@ya.ru>
2022-08-13 12:03:30 +0300

    Задача1 третьего семинара

---
+++ Seminar3/seminar/Homework/Задача1.cs
@@ -0,0 +1,12 @@
+﻿// Задача 1
+
+
+Console.WriteLine("Введите трехзначное число ");
+
+int numberX = Convert.ToInt32(Console.ReadLine());
+
+int a = numberX/100;
+int b = (numberX % 100) / 10;
+int c = numberX % 10;
+
+Console.WriteLine(b + " - является второй цифрой этого числа");
