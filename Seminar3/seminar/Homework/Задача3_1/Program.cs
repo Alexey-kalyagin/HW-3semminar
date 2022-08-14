378c5247188a3ac7171d3659127313669ce6b3d1
Alex <alexey.kalyagin@ya.ru>
2022-08-14 13:20:23 +0300

    Задача3

---
+++ Seminar3/seminar/Homework/Задача3_1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 3
+
+int[] array = {1, 2, 3, 4, 5, 6, 7};
+int n = array.Length;
+int sataday = 6;
+int sunday = 7;
+
+Console.WriteLine("Введите чиcло дня недели");
+
+int day = Convert.ToInt32(Console.ReadLine());
+   
+
+if (day == sunday)
+{
+    Console.WriteLine("Выходной");
+} 
+else
+{
+    if (day == sataday)
+    {
+        Console.WriteLine("Выходной");
+    }
+    else
+    {
+       if (day > n)
+       {
+        Console.WriteLine("такого дня недели нет)");
+       }
+       else
+       {
+       Console.WriteLine("Рабочий день"); 
+       }
+    }
+}    
