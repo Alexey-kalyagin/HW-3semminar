69e4d65fdf300e7b162e5208d742a6ffdfe42f97
Alex <alexey.kalyagin@ya.ru>
2022-08-15 22:09:24 +0300

    Решил задачу4

---
+++ Seminar3/seminar/Homework/Задача4/Задача4.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом.
+
+Console.WriteLine("Введите число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int numberA = 0;
+int numberC = 0;
+int numberD = numberB;
+
+while(numberD > 0)
+{
+    numberA = numberD % 10;
+    numberD = numberD / 10;
+    numberC = numberC * 10;
+    numberC = numberC + numberA;
+}
+if (numberC == numberB)
+{
+    Console.WriteLine(numberB + " - Число является полиндромом");
+}
+else
+{
+    Console.WriteLine(numberB + " - Число НЕ является полиндромом");
+}
