4717e1877bfd78c8bc4b6d7a385184b5c64887fe
Alex <alexey.kalyagin@ya.ru>
2022-08-19 19:11:18 +0300

    задача с точками

---
+++ Seminar3/seminar/Homework/Задача6/Program.cs
@@ -0,0 +1,21 @@
+﻿double [] arr1 = new double[3];
+double [] arr2 = new double[3];
+double sum = 0;
+
+Console.WriteLine("Введите координаты первой точки");
+for (int i = 0; i < 3; i++)
+{
+    arr1[i] = Convert.ToDouble(Console.ReadLine());
+}
+
+Console.WriteLine("Введите координаты второй точки");
+for (int i = 0; i < 3; i++)
+{
+    arr2[i] = Convert.ToDouble(Console.ReadLine());
+}
+
+for (int i = 0; i < 3; i++)
+{
+    sum+= Math.Pow((arr1[i] - arr2[i]),2);
+}
+Console.WriteLine(Math.Sqrt(sum));
