94cce463ab3628519efcb3e6d5b2fa2bf431640d
Alex <alexey.kalyagin@ya.ru>
2022-08-19 17:58:06 +0300

    через стринг

--- Seminar3/seminar/Homework/Задача5/задача5.cs
+++ Seminar3/seminar/Homework/Задача5/задача5.cs
@@ -1,31 +1,10 @@
 ﻿// Напишите программу, которая принимает на вход число (N)
 // и выдаёт таблицу кубов чисел от 1 до N.
 
-Console.WriteLine("Введите число");
-
-int numberN = Convert.ToInt32(Console.ReadLine());
-
-int[] arr = new int [numberN];
-
-int g = 1;
-
-
-void PrintArray(int[] array)
-{
-    int count = array.Length;
-
-    for(int i = 0; i < count; i++)
-    {   
-        int [] arr = {i};              
-        
-        for(int j = 0; j < count; g = g++)
-        {
-          j = numberN - (numberN - g);  
-          array[i] = j;
-        }
-    
-    Console.WriteLine(array[3]);
-    } 
-
-}
-PrintArray(arr);
+          
+         
+          
+          string numberN = Console.ReadLine();
+                    
+          Console.WriteLine(numberN[0]*numberN[0]*numberN[0]);
+          
