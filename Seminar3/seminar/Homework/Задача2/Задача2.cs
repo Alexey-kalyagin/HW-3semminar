cdc15fbcfc52610d063c15522bc3dee335fa8a54
Alex <alexey.kalyagin@ya.ru>
2022-09-02 15:45:10 +0300

    Задача41

---
+++ Seminar3/seminar/Homework/Задача2/Задача2.cs
@@ -0,0 +1,40 @@
+﻿// Задача2
+
+
+Console.WriteLine("Введите число ");
+
+int numberB = Convert.ToInt32(Console.ReadLine());
+int numberA = 0;
+int numberC = 0;
+int[] array = new int[1];
+while (numberB > 0)
+{
+    numberA = numberB % 10;
+    numberB = numberB / 10;
+    numberC = (numberC * 10);
+    numberC = numberC + numberA;
+    void FillArray(int[] collection)
+    {
+        int length = collection.Length;
+        int index = 0;
+        while (index < length)
+        {
+            collection[index] = numberA;
+            index++;
+        }
+    }
+    void PrintArray(int[] col)
+    {
+        int count = col.Length;
+        int position = 0;
+        while (position < count)
+        {
+            Console.WriteLine(col[position]);
+            position++;
+        } 
+    }
+    FillArray(array);
+    PrintArray(array); 
+}
+Console.WriteLine(numberC);
+
---
+++ semminar5/seminar/Homework/Задача41/Program.cs
@@ -0,0 +1,30 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+
+Console.WriteLine ("Введите числа массива через пробел");
+string elem = Console.ReadLine();
+int []array = GetArrayFromString(elem);
+Console.WriteLine (string.Join(" ", GetArrayFromString(elem)));
+int subzero = 0;
+int[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int [] res = new int [nums.Length];
+    for (int i=0; i<nums.Length; i++)
+    {
+        res [i] = int.Parse(nums[i]);
+    }  
+return res;
+}
+
+for (int i=0; i<array.Length; i++)
+{
+    if (array[i]<0)
+    {
+        subzero=subzero+1;
+    }
+    
+}
+Console.WriteLine(subzero);
