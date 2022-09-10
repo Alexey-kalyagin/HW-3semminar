35cac904af64fffb6c48e41edf407eda12bf297c
Alex <alexey.kalyagin@ya.ru>
2022-09-10 12:33:37 +0300

    сортирование max->min

---
+++ semminar5/seminar/Classwork/sort/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+
+int [] array = {1,6,8,5,10,7};
+
+void PrintArray (int [] array)
+{   
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} {" "}");
+    }
+    Console.WriteLine();
+}
+
+
+void SelectionSort (int[] array)
+{   
+    int count = array.Length;
+    for (int i = 0; i < count-1; i++)
+    {
+        int maxPos = i;
+        for (int j = i+1; j < array.Length; j++)
+        {
+            if (array[j]>array[maxPos]) maxPos=j;
+        }
+        int temp = array[i];
+        array[i] = array[maxPos];
+        array[maxPos] = temp;
+    }
+}
+
+PrintArray (array);
+Console.WriteLine();
+SelectionSort(array);
+Console.WriteLine();
+//PrintArray (array);
